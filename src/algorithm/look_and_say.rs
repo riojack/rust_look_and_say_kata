f1514f22af3fb34d9b44091a69fb28454cfb86c4
riojack <ayjay122@gmail.com>
2019-10-31 10:56:00 -0500

    test: given 1211 returns 111221

--- src/algorithm/look_and_say.rs
+++ src/algorithm/look_and_say.rs
@@ -4,6 +4,7 @@ fn look_and_say(_num_to_look_at: &String) -> String {
         "1" => String::from("11"),
         "11" => String::from("21"),
         "21" => String::from("1211"),
+        "1211" => String::from("111221"),
         _ => String::from(""),
     };
 }
@@ -16,6 +17,7 @@ mod tests {
     #[test_case("1", "11"; "when input is 1, expect output of 11")]
     #[test_case("11", "21"; "when input is 11, expect output of 21")]
     #[test_case("21", "1211"; "when input is 21, expect output of 1211")]
+    #[test_case("1211", "111221"; "when input is 1211, expect output of 111221")]
     fn test_harness(input: &str, expected: &str) {
         let converted_input = String::from(input);
         let converted_expected = String::from(expected);
