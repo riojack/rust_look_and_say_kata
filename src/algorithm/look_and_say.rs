7f868c26778357962b38cf33e21ef3cd9c5623a8
riojack <ayjay122@gmail.com>
2019-10-31 10:07:52 -0500

    test: given 1 returns 11

--- src/algorithm/look_and_say.rs
+++ src/algorithm/look_and_say.rs
@@ -1,11 +1,18 @@
-fn hello_world() {
-    println!("Hello, world!")
+#[allow(dead_code,unused_variables)]
+fn look_and_say(_num_to_look_at: &String) -> String {
+    return String::from("11");
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::algorithm::look_and_say::look_and_say;
+
     #[test]
-    fn two_equals_two() {
-        assert_eq!(2, 2);
+    fn returns_11_when_given_1() {
+        let num = String::from("1");
+        let expected = String::from("11");
+        let actual = look_and_say(&num);
+
+        assert_eq!(&expected, &actual);
     }
 }
