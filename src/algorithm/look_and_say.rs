5ba0128275833e3153930b565aaa6a85d7b5651c
riojack <ayjay122@gmail.com>
2019-10-31 10:29:24 -0500

    test: given 11 returns 21

--- src/algorithm/look_and_say.rs
+++ src/algorithm/look_and_say.rs
@@ -1,18 +1,27 @@
-#[allow(dead_code,unused_variables)]
+#[allow(dead_code, unused_variables)]
 fn look_and_say(_num_to_look_at: &String) -> String {
-    return String::from("11");
+
+    return match _num_to_look_at.as_ref() {
+        "1" => String::from("11"),
+        "11" => String::from("21"),
+        _ => String::from("")
+    };
 }
 
 #[cfg(test)]
 mod tests {
+    use test_case::test_case;
     use crate::algorithm::look_and_say::look_and_say;
 
-    #[test]
-    fn returns_11_when_given_1() {
-        let num = String::from("1");
-        let expected = String::from("11");
-        let actual = look_and_say(&num);
+    #[test_case("1", "11"; "when input is 1, expect output of 11")]
+    #[test_case("11", "21"; "when input is 11, expect output of 21")]
+    fn test_harness(input: &str, expected: &str) {
+        let converted_input = String::from(input);
+        let converted_expected = String::from(expected);
 
-        assert_eq!(&expected, &actual);
+        let actual = look_and_say(&converted_input);
+
+        assert_eq!(&converted_expected, &actual);
     }
+
 }
